7389c160909efd59c8f4c9658a276471cecc0ba1
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2025-01-15 19:20:15 +0100

    step 5 done

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import pino from 'pino-http';
 import cors from 'cors';
 
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts } from './services/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -18,9 +19,20 @@ export const setupServer = () => {
             },
         }),
     );
+
     app.get('/', (req, res) => {
         res.json({
             message: 'Hello world!',
+        });
+    });
+
+    app.get('/contacts', async (req, res) => {
+        const contacts = await getAllContacts();
+
+        res.status(200).json({
+            status: 200,
+            message: "Successfully found contacts!",
+            data: contacts,
         });
     });
     
