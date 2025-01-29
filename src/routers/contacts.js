204b3d073a2a3d2adb78646b5aea65b36bfa566d
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2025-01-29 14:40:50 +0100

    step 7 done

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -10,8 +10,11 @@ import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 import { isValidId } from "../middlewares/isValidId.js";
+import { authenticate } from '../middlewares/authenticate.js';
 
 const contactsRouter = Router();
+
+contactsRouter.use(authenticate);
 
 contactsRouter.get('/', ctrlWrapper(getContactsController));
 
