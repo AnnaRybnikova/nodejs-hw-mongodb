7e05d5714624f9891e70dd647807d9703e3afe39
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2025-01-29 13:35:43 +0100

    step 1-2 done

---
+++ src/db/models/user.js
@@ -0,0 +1,26 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            email: true,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    },
+);
+
+export const UserCollection = model('user', userSchema);
